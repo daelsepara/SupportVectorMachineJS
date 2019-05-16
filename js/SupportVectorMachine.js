428248b43f56d46a4a5fcf30f447f6f436ce029b
Dael <dael.separa@gmail.com>
2019-05-16 20:34:25 +0800

    current progress: load training data

--- js/SupportVectorMachine.js
+++ js/SupportVectorMachine.js
@@ -4,5 +4,144 @@ angular
 	.module('DeepLearning', ['ngWebworker', 'ngFileSaver', 'd3'])
 	.controller('SupportVectorMachineController', ['$scope', 'Webworker', 'FileSaver', 'Blob', function($scope, Webworker, FileSaver, Blob) {
 	
+		$scope.Classification = [];
+		$scope.Prediction = [];
+		
+		$scope.TrainingData = [];
+		$scope.Output = [];
+		$scope.Inputs = 0;
+		$scope.Categories = 0;
+		$scope.Items = 0;
+
+		$scope.Training = false;
+		
+		$scope.SelectedFile = {};
+		$scope.TestFile = {};
+		$scope.ModelFile = {};
+		
+		$scope.TestData = [];
+		$scope.Samples = 0;
+		
+		$scope.DelimiterNames = ["Tab \\t", "Comma ,", "Space \\s", "Vertical Pipe |", "Colon :", "Semi-Colon ;", "Forward Slash /", '/', "Backward Slash \\"];
+		$scope.Delimiters = ['\t', ',', ' ', '|', ':', ';', '/', '\\'];
+		$scope.delimiter = $scope.DelimiterNames[0];
+		$scope.SelectedDelimiter = 0;
+
+		$scope.SelectDelimiter = function() {
+			
+			var i = $scope.DelimiterNames.indexOf($scope.delimiter);
+			
+			$scope.SelectedDelimiter = i + 1;
+		}
+
+		$scope.ReadTrainingData = function() {
+			
+			$scope.TrainingData = [];
+			$scope.Output = [];
+			$scope.Items = 0;
+			$scope.Categories = 0;
+			$scope.Inputs = 0;
+			
+			var reader = new FileReader();
+
+			reader.onload = function(progressEvent) {
+
+				$scope.$apply(function() {
+					
+					var lines = reader.result.split('\n');
+					
+					var delimiter = $scope.SelectedDelimiter > 0 ? $scope.Delimiters[$scope.SelectedDelimiter - 1] : "\t";
+					
+					for (var line = 0; line < lines.length; line++) {
+
+						var tmp = [];
+
+						var tokens = lines[line].trim().split(delimiter);
+
+						if (tokens.length > 0) {
+							
+							if (line == 0 && tokens.length > 1) {
+								
+								$scope.Inputs = tokens.length - 1;
+							}
+							
+							if (tokens.length > 1) {
+								
+								var item = parseInt(tokens[tokens.length - 1]);
+								
+								$scope.Categories = Math.max($scope.Categories, item);
+								
+								$scope.Output.push([item]);
+							}
+								
+							for (var i = 0; i < tokens.length - 1; i++) {
+								
+								tmp.push(parseFloat(tokens[i]));
+							}
+							
+							if (tmp.length > 0) {
+								
+								$scope.TrainingData.push(tmp);
+								
+								$scope.Items++;
+							}
+						}
+					}
+					
+					$scope.fileContent = reader.result.trim();
+					
+				});
+			}
+
+			if ($scope.SelectedFile.name != undefined) {
+				
+				reader.readAsText($scope.SelectedFile);
+			}
+		};
+
 	
-	}]);
+	}]).directive("inputBind", function() {
+		
+		return function(scope, elm, attrs) {
+			
+			elm.bind("change", function(evt) {
+				
+				scope.$apply(function(scope) {
+					
+					scope[ attrs.name ] = evt.target.files;
+					scope['Items'] = 0;
+					scope['Categories'] = 0;
+					scope['Inputs'] = 0;
+					scope['SelectedFile'] = evt.target.files[0];
+				});
+			});
+		};
+		
+	}).directive("testBind", function() {
+		
+		return function(scope, elm, attrs) {
+			
+			elm.bind("change", function(evt) {
+				
+				scope.$apply(function(scope) {
+					
+					scope[ attrs.name ] = evt.target.files;
+					scope['Samples'] = 0;
+					scope['TestFile'] = evt.target.files[0];
+				});
+			});
+		};
+	}).directive("modelBind", function() {
+		
+		return function(scope, elm, attrs) {
+			
+			elm.bind("change", function(evt) {
+				
+				scope.$apply(function(scope) {
+					
+					scope[ attrs.name ] = evt.target.files;
+					scope['ModelFile'] = evt.target.files[0];
+				});
+			});
+		};
+	});
